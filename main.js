0e3f0e01a42ccc0d72c02495295b03de0b32e15c
Johanna Huaraca <86489770+Jahe01@users.noreply.github.com>
2023-06-16 17:54:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-console.log("Hello, Word!");
-//node "nombre del .js"
+function generarArbolNavidad(filas) {
+  for (let i = 0; i < filas; i++) {
+    let espacios = ' '.repeat(filas - i);
+    let asteriscos = '*'.repeat(i * 2 + 1);
+    console.log(espacios + asteriscos);
+  }
+
+  // Dibuja el tronco del árbol
+  let espacios = ' '.repeat(filas - 1);
+  console.log(espacios + '|');
+}
+
+// Llama a la función para generar un árbol con 5 filas
+generarArbolNavidad(5);
